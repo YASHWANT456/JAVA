61e2c8507bfebab7d76645ea08c1b0afb4fbc488
Yashwant <rishumishraindia@gmail.com>
2022-08-14 11:55:42 +0530

    Solid Rhombus

--- Patterns/Pattern10.java
+++ Patterns/Pattern10.java
@@ -2,6 +2,30 @@ import java.util.*;
 
 public class Pattern10 
 {
-    
+    public static void solidRhombus(int n)
+    {
+        for(int i = 1;i<=n;i++)
+        {
+            for(int j = 1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+
+            }
+            for(int j = 1;j<=n;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value of no. of rows : ");
+        int n = sc.nextInt();
+        solidRhombus(n);
+
+    }
+
     
 }
