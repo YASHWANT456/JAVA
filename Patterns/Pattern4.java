956ea33abe2f6161e1d2f5a11c71847b744fcd4d
Yashwant <rishumishraindia@gmail.com>
2022-08-14 10:17:39 +0530

    Character Pattern

---
+++ Patterns/Pattern4.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Pattern4 
+{
+    public static void characterPattern(int n)
+    {
+        char ch = 'A';
+        for(int i = 1;i<=n;i++)
+        {
+            for(int j = 1;j<=i;j++)
+            {
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String arg[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of no. of rows : ");
+        int n = sc.nextInt();
+        characterPattern(n);
+
+    }
+    
+}
