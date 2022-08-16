cded01461d8b0973bd99176ff0e653d15f58c2c2
Yashwant <rishumishraindia@gmail.com>
2022-08-16 10:33:00 +0530

    Hollow Square star with Diagonal

--- Patterns/Pattern16.java
+++ Patterns/Pattern16.java
@@ -12,7 +12,7 @@ public class Pattern16
             }
             for(int j = 1;j<=i;j++)
             {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println(" ");
         }
